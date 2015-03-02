02ea68eb3c1761314ec9eb3fb3ad2eb543d436cd
MaskedRpg <masked.rpg@gmail.com>
2015-03-02 00:32:42 -0300

    Update and rename MBS.dll to MBS.dll/crypt.cpp

---
+++ MBS.dll/crypt.cpp
@@ -0,0 +1,17 @@
+#include "crypt.h"
+
+using namespace std;
+
+MBS_API void Encrypt(const int* text, const int key, const long tsize, int* buffer) {
+
+	for (int i = 0; i < tsize; i++) {
+		buffer[i] = text[i] + key;
+	}
+}
+
+MBS_API void Decrypt(const int* text, const int key, const long tsize, int* buffer) {
+
+	for (int i = 0; i < tsize; i++) {
+		buffer[i] = text[i] - key;
+	}
+}
